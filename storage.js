2c2e9546524507dff8b4fba08fcff84db726ae96
Nick <nicholasgin@gmail.com>
2021-11-20 21:02:41 -0500

    added checkbox value to local storage

--- storage.js
+++ storage.js
@@ -1,4 +1,55 @@
-chrome.storage.local.set({key:value}, function(){
+
+//environment
+chrome.storage.local.set({c1:'env1'}, function(){
+    console.log(value)
+});
+
+chrome.storage.local.set({c2:'env2'}, function(){
+    console.log(value)
+});
+chrome.storage.local.set({c3:'env3'}, function(){
+    console.log(value)
+});
+chrome.storage.local.set({c4:'env4'}, function(){
+    console.log(value)
+});
+chrome.storage.local.set({c5:'env5'}, function(){
+    console.log(value)
+});
+
+// humanitarian
+chrome.storage.local.set({c1:'hum1'}, function(){
+    console.log(value)
+});
+
+chrome.storage.local.set({c2:'hum2'}, function(){
+    console.log(value)
+});
+chrome.storage.local.set({c3:'hum3'}, function(){
+    console.log(value)
+});
+chrome.storage.local.set({c4:'hum4'}, function(){
+    console.log(value)
+});
+chrome.storage.local.set({c5:'hum5'}, function(){
+    console.log(value)
+});
+
+//social
+chrome.storage.local.set({c1:'soc1'}, function(){
+    console.log(value)
+});
+
+chrome.storage.local.set({c2:'soc2'}, function(){
+    console.log(value)
+});
+chrome.storage.local.set({c3:'soc3'}, function(){
+    console.log(value)
+});
+chrome.storage.local.set({c4:'soc4'}, function(){
+    console.log(value)
+});
+chrome.storage.local.set({c5:'soc5'}, function(){
     console.log(value)
 });
 
